7bc4592a43d5a87b2109e7be91eed779f6fff28a
Villacarlos06 <Villacarlosalcorace@gmail.com>
2024-10-03 11:31:13 +0800

    task1

---
+++ task1.py
@@ -0,0 +1,7 @@
+num = int(input("Enter a number"))
+faveFruits = []
+
+for data in range (num):
+     fruit = input("what is your favorite fruit?")
+     faveFruit. append (Fruit)
+print (faveFruits)     
